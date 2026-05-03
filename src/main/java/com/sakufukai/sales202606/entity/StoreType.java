8227bc85969bf845688e79a9e4a6a838c6401a01
kindaiyt <2533340439b@kindai.ac.jp>
2026-05-04 00:55:07 +0900

    店舗一覧を表示する際に、店舗分類を合わせて表示するように変更

--- src/main/java/com/sakufukai/sales202606/entity/StoreType.java
+++ src/main/java/com/sakufukai/sales202606/entity/StoreType.java
@@ -1,6 +1,16 @@
 package com.sakufukai.sales202606.entity;
 
 public enum StoreType {
-    STUDENT, // 学生の模擬店舗
-    GENERAL  // 一般店舗
+    STUDENT("学生店舗"), // 学生の模擬店舗
+    GENERAL("一般店舗"); // 一般店舗
+
+    private final String displayName;
+
+    StoreType(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
